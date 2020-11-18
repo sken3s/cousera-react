d1e41ce662a34378d48f3574997374895be7222d
Skenos Peniel <skenos3s@gmail.com>
2020-11-18 20:57:11 +0530

    presentational and container components

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -29,7 +29,12 @@ class Dishdetail extends Component {
         <li key={c.id}>
           <li>{c.comment}</li>
           <li>
-            --{c.author}, {c.date}
+            --{c.author},{" "}
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+            }).format(new Date(Date.parse(c.date)))}
           </li>
         </li>
       );
@@ -45,12 +50,14 @@ class Dishdetail extends Component {
   render() {
     if (this.props.dish != null) {
       return (
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.props.dish)}
-          </div>
-          <div className="col-12 col-md-5 m-1">
-            {this.renderComments(this.props.dish)}
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-5 m-1">
+              {this.renderDish(this.props.dish)}
+            </div>
+            <div className="col-12 col-md-5 m-1">
+              {this.renderComments(this.props.dish)}
+            </div>
           </div>
         </div>
       );
--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -7,27 +7,17 @@ import {
   CardBody,
   CardTitle,
 } from "reactstrap";
-import Dishdetail from "./DishdetailComponent";
 
 class Menu extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      selectedDish: null,
-    };
-  }
-  onDishSelect(dish) {
-    this.setState({ selectedDish: dish });
   }
 
-  renderDish(dish) {
-    return <Dishdetail dish={dish} />;
-  }
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={() => this.onDishSelect(dish)}>
+          <Card onClick={() => this.props.onClick(dish.id)}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
               <CardTitle heading>{dish.name}</CardTitle>
@@ -39,7 +29,6 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="row">{menu}</div>
-        <div className="row">{this.renderDish(this.state.selectedDish)}</div>
       </div>
     );
   }
